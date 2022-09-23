f11621688823ab227478a2cafb210bebf956ad3e
Theo <ekunnusitheophilus@gmail.com>
2022-09-23 01:14:27 +0100

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,14 +8,13 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t size = 0;
-    
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	size_t nodes = 0;
 
-    size += tree->left != NULL || tree->right != NULL ? 1 : 0;
-    size += binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
-    return (size);
+	if (tree)
+	{
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
 }
